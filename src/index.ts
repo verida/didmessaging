b1c1d893a77bb9f75b85b023ae5f33295f44d269
nick-verida <nick@verida.io>
2023-11-29 11:54:42 +1030

    take file for DIDs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
+
 import { Client, Context, Network } from '@verida/client-ts';
 import { EnvironmentType, IMessaging } from '@verida/types';
 import { AutoAccount } from '@verida/account-node';
 import { Wallet } from 'ethers';
 import PromptSync  from 'prompt-sync';
-import { Command } from 'commander';
+import { Command, Option } from 'commander';
 import 'dotenv/config'
+import * as fs from 'fs';
 
 
 export default class DIDMessagingApp {
-
-  #veridaContext: Context | undefined;
   veridaContext: Context | undefined;
   veridaAccount: AutoAccount | undefined;
   veridaMessaging: IMessaging | undefined;
@@ -59,49 +59,65 @@ export default class DIDMessagingApp {
     })
 
     await client.connect(this.veridaAccount);
-    
 
-    this.veridaContext = await client.openContext("DID Message Sender", true) as Context;
+
+    this.veridaContext = await client.openContext("Verida Official Messages", true) as Context;
 
   }
   
   private keyPhraseToKey() {
     const prompt = PromptSync();
     const phrase = prompt("Please enter the seed phrase:");
-    const wallet = Wallet.fromPhrase(phrase) 
+    // const wallet = Wallet.fromPhrase(phrase) v6 syntax
+
+    const wallet = Wallet.fromMnemonic(phrase)
+
     console.log("Private key:", wallet.privateKey);
   }
 
-  private async sendMessage() {
-    await this.connect();
-    const messaging = await this.veridaContext.getMessaging();
+  private async sendMessage(didfile: string) {
+    if (didfile) {
+      const didlist = fs.readFileSync(didfile, 'utf-8');
 
+      console.log("Connecting to Verida...")
+      await this.connect();
+      console.log("Getting messaging...")
+      const messaging = await this.veridaContext.getMessaging();
+  
+      // set the subject
+      const subject = "Message from Nick!"
+      // we need to set it to the correct message type
+      const messageType = "inbox/type/message"
+  
 
-    // set the subject
-    const subject = "Message from Nick!"
-    // we need to set it to the correct message type
-    const messageType = "inbox/type/message"
+      const data = {
+        data: [{
+          subject:subject,
+          message:"test message"
+        }]
+      }    
 
-    const data = {
-      data: [{
-        subject:subject,
-        message:"test message"
-      }]
-    }    
+      const dids = didlist.split(/\n/)
 
-    //onst toDID = "did:vda:testnet:0xf080913daa2f1F2F93dfF494a2F3E6e1A2e2C492";
-    //const toDID = "did:vda:testnet:0xb362351168D370b174E0fD3Feec93C4E6d2938e2"
-    const toDID = "did:vda:testnet:0x6Aef8B78D47316a61e10703d4dAe5F0a34516a3B"
-
-    const config = {
-      recipientContextName: "Verida: Vault",
-      
-      did: toDID
+      for (let i = 0; i < dids.length; i++) {
+        const config = {
+          recipientContextName: "Verida: Vault",
+          
+          did: dids[i]
+        }
+    
+        console.log(`Sending message ${i + 1}/${dids.length} to ${dids[i]}`);
+        await messaging.send(dids[i], messageType, data, subject, config);
+      }
+  
+      console.log("Messages sent.")
+      console.log("Closing context...")
+      await this.veridaContext.close();
+      //await this.veridaContext.disconnect();
+  
     }
 
-    await messaging.send(toDID, messageType, data, subject, config);
 
-    await this.veridaContext.close();
 
     return;
   }
@@ -114,6 +130,7 @@ export default class DIDMessagingApp {
       .option("-nd --newdid", "Create a new DID to send messages from")
       .option("-kp --keyphrase", "Get a private key from a key phrase")
       .option("-s --send", "Send a message")
+      .addOption(new Option("-df --didfile <filename>", "File of DIDs to send to formatted one-per-line").preset("didfile.txt"))
       .parse(process.argv);
     
     const options = program.opts();
@@ -125,9 +142,10 @@ export default class DIDMessagingApp {
     } else if (options['keyphrase']) {
       await this.keyPhraseToKey()
     } else if (options['send']) {
-      await this.sendMessage()
+      await this.sendMessage(options['didfile'])
     }
- 
+
+    return
   }
 }
 
